56f6828f12e95dc21baead9211bab2e839504e6b
cr8br0ze <118150296+cr8br0ze@users.noreply.github.com>
2022-12-04 22:12:00 -0800

    Add files via upload

---
+++ lab10/lab10.scm
@@ -0,0 +1,29 @@
+(define (over-or-under num1 num2) 
+    (cond 
+        ((> num1 num2) 1)
+        ((> num2 num1) -1)
+        (else 0))
+    )
+
+(define (make-adder num) 
+    (lambda (inc) (+ num inc)))
+
+(define (composed f g)
+    (lambda (x) (f(g x)))
+    )
+
+(define lst (cons
+                (cons 1 nil)
+                (cons 2 (cons (cons 3 
+                                    (cons 4 nil))
+                                (cons 5 nil)
+                                )))
+                                    )
+
+(define (duplicate lst) 
+                        (cond 
+                            ((null? lst) lst)
+                            (else (cons (car lst) (cons (car lst) (duplicate (cdr lst))))
+                            )
+                            )
+                            )
